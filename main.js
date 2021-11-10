c9267880705e6f342133d0bb2866cdc06eae7b5f
RcA2650 <remeshcanandan14@gmail.com>
2021-11-11 00:16:14 +0530

    More changes, ID...

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
 var taskName = document.getElementById("add-box");
 var addBtn = document.getElementById("add-btn");
-let pendingN = 0;
-let completedN = 0;
+count=0;
+let pendingNumber = 0;
+let completedNumber = 0;
+
 let pending = {
     
+}
+
+let completed = {
+
 }
 
 function addTask(taskName)
@@ -16,17 +22,24 @@ function addTask(taskName)
     }
     else
     {
-        var t = document.createTextNode(newItem);
         var doneBox = document.createElement("input");
+        doneBox.id = `newItem-${count+1}`;
+        var label = document.createElement("label");
+        label.innerText = newItem;
+        //var t = document.createTextNode(lbl);
         doneBox.type = "checkbox";
         newTodo.appendChild(doneBox);
-        newTodo.appendChild(t);
+        newTodo.appendChild(lbl);
+        //newTodo.appendChild(t);
         document.getElementById("tlist").appendChild(newTodo);
         document.getElementById("add-box").value = "";
-        pending[pendingN] = newItem;
-        console.log(pending);
-        pendingN+=1;
-        
+        pending[pendingNumber] = newItem;
+        //console.log(pending);
+        pendingNumber+=1;
     }
     
+}
+function doneTask()
+{
+    console.log(this.value); 
 }
