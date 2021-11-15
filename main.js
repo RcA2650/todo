49964bfd7cb7e7f8c92a7e81e52ef1a2c6d1a75f
RcA2650 <remeshcanandan14@gmail.com>
2021-11-16 01:03:47 +0530

    Todo adding and checking working

--- main.js
+++ main.js
@@ -12,9 +12,15 @@ let completed = {
 
 }
 
+
 function addTask(taskName)
 {
+    //var divForItem = document.createElement("div");
+    //divForItem.setAttribute("class", `itemDiv-${count+1}`);
     var newTodo = document.createElement("li");
+    newTodo.id = `newItemList-${count}`;
+    
+    //divForItem.appendChild(newTodo);
     var newItem = document.getElementById("add-box").value;
     if(newItem === "")
     {
@@ -22,15 +28,28 @@ function addTask(taskName)
     }
     else
     {
+        
+        //document.getElementsByClassName("list").setAttribute("onclick", "check(event)");
+        // divForItem.setAttribute("onclick", "check(event)");
+        // li.appendChild(newTodo);
         var doneBox = document.createElement("input");
-        doneBox.id = `newItem-${count+1}`;
+        doneBox.id = `newItemBox-${count}`;
+
         var label = document.createElement("label");
+        label.setAttribute("class","strikethrough");
         label.innerText = newItem;
+        count = count +1;
+        label.id = `newItem-${count}`;
+        label.setAttribute("onclick", "check(event)");
+        label.setAttribute("for",`newItemLabel-${count}`);
         //var t = document.createTextNode(lbl);
         doneBox.type = "checkbox";
+        
         newTodo.appendChild(doneBox);
-        newTodo.appendChild(lbl);
+        newTodo.appendChild(label);
+        
         //newTodo.appendChild(t);
+        
         document.getElementById("tlist").appendChild(newTodo);
         document.getElementById("add-box").value = "";
         pending[pendingNumber] = newItem;
@@ -39,7 +58,47 @@ function addTask(taskName)
     }
     
 }
-function doneTask()
+
+// var checkBox = document.querySelector("input[type=checkbox]");
+
+// checkBox.addEventListener('change', function(){
+//     if(this.checked){
+//        comsole.log(document.getElementById(`newItemBox-${count+1}`));
+//     }
+
+// });
+// function check(this)
+// {
+
+// }
+function check(e)
 {
-    console.log(this.value); 
+    // alert(e.target.innerText);
+    // alert(e.target.id);
+    let val = e.target.innerText;
+    let idCompete = e.target.id;
+    document.getElementById(e.target.id).previousSibling.checked=true;
+    // var tempToDo = document.createElement("li");
+    var tempToDo  = document.getElementById(e.target.id).parentElement.cloneNode(true);
+    console.log(document.getElementById(e.target.id).parentElement);
+    document.getElementById(e.target.id).parentElement.remove();
+    console.log(tempToDo);
+
+    document.getElementById("clist").appendChild(tempToDo);
+
+
+    //document.getElementById(e.target.id).setAttribute("style", "text-decoration: line-through");
+   // document.getElementById(e.target.id).parentElement.checked = true;
+    //var newCompleted = document.createElement("li");
+    //newCompleted.appendChild(e.target);
+    
+   // e.target.style.text-decoration : "line-through";
+   // document.getElementById(idCompete);
+    //document.getElementById("clist").appendChild(newTodo);
+
 }
+
+// for(;;)
+// {
+//     document.getElementsByClassName;
+// }
